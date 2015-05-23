22f2da37e8d52e59caf706af9314b869735e59ab
Jiyuan Liu <liujiyuan126@yeah.net>
2015-05-23 21:29:34 +0800

    Wait for sending.

--- ip_forward.c
+++ ip_forward.c
@@ -10,7 +10,7 @@
 #define MAX_BUFFER 2048
 #define MAX_ROUTE_INFO 10
 #define MAX_ARP_SIZE 10
-//#define DEBUG
+#define DEBUG
 
 //the information of the static_routing_table
 struct route_item{
@@ -114,13 +114,53 @@ int find_arp_by_ip(char* ip_addr){
 	return -1;
 }
 
+char* find_route_gateway(char* ip_addr){
+	static char gateway[16];
+	memset(gateway, 0, 16);
+	int i;
+	for(i=0; i<route_item_index; i++){
+		//Just ignore the input netmask and set it 24 ^_^.
+		if(strncmp(ip_addr, route_info[i].destination, 12) == 0){
+			strncpy(gateway, route_info[i].gateway, 15);
+			return gateway;
+		}
+	}
+	printf("Broken route table.\n");
+	exit(0);
+	return NULL;
+}
+
+void refresh_arp(char* ip_addr, char* mac_addr){
+	if(find_arp_by_ip(ip_addr) != -1){
+		return;
+	}
+	if(arp_item_index == MAX_ARP_SIZE){
+		arp_item_index--;
+	}
+	strcpy(arp_table[arp_item_index].ip_addr, ip_addr);
+	strcpy(arp_table[arp_item_index].mac_addr, mac_addr);
+	arp_item_index++;
+	char* gateway = find_route_gateway(ip_addr);
+	if(find_arp_by_ip(gateway) != -1){
+		return;
+	}
+	int i = arp_item_index;
+	if(arp_item_index == MAX_ARP_SIZE){
+		i = i-2;
+	}
+	strcpy(arp_table[i].ip_addr, gateway);
+	strcpy(arp_table[i].mac_addr, mac_addr);
+}
+
 int main(){
 	//Init tables and variables.
 	int i;
 	int sock_fd;
 	int n_read = 0;
-	int route_index = -1;
-	int arp_index = -1;
+	int src_route_index = -1;
+	int des_route_index = -1;
+	int src_arp_index = -1;
+	int des_arp_index = -1;
 	char buffer[MAX_BUFFER];
 	char type[4];
 	char src_ip[16];
@@ -130,6 +170,7 @@ int main(){
 	char* p;
 	char* ip_head;
 	char* eth_head;
+	char* gateway;
 	unsigned count=0;
 	memset(src_ip, 0, 16);
 	memset(des_ip, 0, 16);
@@ -178,6 +219,9 @@ int main(){
 		count++;
 		eth_head = buffer;
 		p = eth_head;
+#ifdef DEBUG
+		printf("Start reading eth head.\n");
+#endif
 		sprintf(des_mac, "%02x:%02x:%02x:%02x:%02x:%02x",
 			p[0]&0xff, p[1]&0xff,
 			p[2]&0xff, p[3]&0xff,
@@ -195,7 +239,60 @@ int main(){
 			printf("Unrecognized datagram: 0x%s\n", type);
 			continue;
 		}
+#ifdef DEBUG
+		printf("Finish reading eth head.\n");
+		printf("Destination MAC: %s\n", des_mac);
+		printf("Source MAC:      %s\n", src_mac);
+		printf("Start reading IP head.\n");
+#endif
 		//Read the IP head.
+		ip_head = eth_head+14;
+		p = ip_head+12;
+		sprintf(src_ip, "%d.%d.%d.%d", 
+			p[0]&0xff, p[1]&0xff,
+			p[2]&0xff, p[3]&0xff
+		);
+		p += 4;
+		sprintf(des_ip, "%d.%d.%d.%d",
+			p[0]&0xff, p[1]&0xff,
+			p[2]&0xff, p[3]&0xff
+		);
+#ifdef DEBUG
+		printf("Finish reading IP head.\n");
+		printf("Destination IP: %s\n", des_ip);
+		printf("Source IP:      %s\n", src_ip);
+		printf("Start refreshing ARP table and checking local or not.\n");
+#endif
+		refresh_arp(src_ip, src_mac);
+		src_arp_index = find_arp_by_ip(src_ip);
+		des_arp_index = find_arp_by_ip(des_ip);
+		if( src_arp_index == 1 ||
+			src_arp_index == 2 ||
+			des_arp_index == 1 ||
+			des_arp_index == 2 ){
+			//This is a local packet.
+			continue;
+		}
+#ifdef DEBUG
+		printf("This is not local.\n");
+		printf("Start checking the static ip table.\n");
+#endif
+		gateway = find_route_gateway(des_ip);
+		des_arp_index = find_arp_by_ip(gateway);
+		if(des_arp_index == 1 || des_arp_index == 2){
+			des_arp_index = find_arp_by_ip(des_ip);
+		}
+		if(des_arp_index == -1){
+			strncpy(des_mac, "ff:ff:ff:ff:ff:ff", 17);
+		} else {
+			strncpy(des_mac, arp_table[des_arp_index].mac_addr, 17);
+		}
+#ifdef DEBUG
+		printf("Get destination gateway and MAC.\n");
+		printf("Gateway: %s\n", gateway);
+		printf("MAC:     %s\n", des_mac);
+		printf("Start sending.\n");
+#endif
 	}
 	return count;
 }
